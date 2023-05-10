54de556758281590837649a45203d9828922c6bf
ColombiaAmaArte <130434484+ColombiaAmaArte@users.noreply.github.com>
2023-05-10 16:27:06 -0500

    Update producto.js

--- producto.js
+++ producto.js
@@ -20,7 +20,7 @@ productsArray.forEach(obra => {
         <div class="container px-4 px-lg-5 my-5">
         <div class="row gx-4 gx-lg-5 align-items-center">
         <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0"
-        src="${obra.imagenes.img1}" alt="..."></div>
+        src="${obra.img1}" alt="..."></div>
         <div class="col-md-6">
         <h1 class="display-5 fw-bolder">${obra.titulo}</h1>
         <div class="fs-5 mb-5">
